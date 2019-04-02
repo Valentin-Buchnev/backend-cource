56e29fbecd339874dc0c6f4ffab772e5e09439b9
Valentin Buchnev <buchnev.valentin@gmail.com>
2019-04-03 01:30:30 +0300

    added email sending verification (it is working!!!)

--- mail-sender/receive.py
+++ mail-sender/receive.py
@@ -4,6 +4,11 @@ import traceback, sys
 
 import smtplib
 
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+from flask import Flask
+
 conn_params = pika.ConnectionParameters('rabbit', 5672)
 connection = pika.BlockingConnection(conn_params)
 channel = connection.channel()
@@ -14,11 +19,20 @@ print("Waiting for messages. To exit press CTRL+C")
 
 def callback(ch, method, properties, body):
     email, confirm_url = body.split(" ")
+
+    msg = MIMEMultipart()
+    msg['From'] = 'a.einstein1488@gmail.com'
+    msg['To'] = email
+    msg['Subject'] = "Verification"
+    
+    msg.attach(MIMEText(confirm_url, 'plain'))
+
     try:
-        smtpObj = smtplib.SMTP('smtp.mail.ru', 465)
+        smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
         smtpObj.starttls()
-        smtpObj.login('buchnev.kozinov.backend@mail.ru', 'backend2019')
-        smtpObj.sendmail('buchnev.kozinov.backend@mail.ru', email, confirm_url)
+        smtpObj.login('a.einstein1488@gmail.com', 'abacaba_1')
+        smtpObj.sendmail('a.einstein1488@gmail.com', 
+                email, msg.as_string())
         smtpObj.quit()
         return True
     except Exception:
