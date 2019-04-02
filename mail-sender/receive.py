7336e37f488f27b533dfab3b09409ec129b95846
Valentin Buchnev <buchnev.valentin@gmail.com>
2019-04-03 00:24:34 +0300

    added email sending

--- mail-sender/receive.py
+++ mail-sender/receive.py
@@ -2,6 +2,8 @@
 import pika
 import traceback, sys
 
+import smtplib
+
 conn_params = pika.ConnectionParameters('rabbit', 5672)
 connection = pika.BlockingConnection(conn_params)
 channel = connection.channel()
@@ -11,7 +13,16 @@ channel.queue_declare(queue='queue', durable=True)
 print("Waiting for messages. To exit press CTRL+C")
 
 def callback(ch, method, properties, body):
-    print(body)
+    email, confirm_url = body.split(" ")
+    try:
+        smtpObj = smtplib.SMTP('smtp.mail.ru', 465)
+        smtpObj.starttls()
+        smtpObj.login('backend.mipt@bk.ru','lolkek123')
+        smtpObj.sendmail("backend.mipt@bk.ru", email, confirm_url)
+        smtpObj.quit()
+        return True
+    except Exception:
+        return False
 
 channel.basic_consume('queue', callback)
 
